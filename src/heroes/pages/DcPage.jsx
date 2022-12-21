14e96172cfaedc3518b98c13cf186567cc81e2e1
Matias Onate <mationate.0@gmail.com>
2022-12-21 18:27:43 -0300

    aplicacion funcionando y rutas creadas

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,11 @@
+import { HeroeList } from "../components/HeroeList"
+
+export const DcPage = () => {
+  return (
+    <>
+      <div>DC Comics</div>
+      <hr />
+      <HeroeList publisher={'DC Comics'}/>
+    </>
+  )
+}
---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,11 @@
+import { HeroeList } from "../components/HeroeList"
+
+export const MarvelPage = () => {
+  return (
+    <>
+      <div>MarvelPage</div>
+      <hr />
+      <HeroeList publisher={'Marvel Comics'}/>
+    </>
+  )
+}
